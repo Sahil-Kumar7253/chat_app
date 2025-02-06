2095626bfc99fbdc9c084f8d8d64a2fdf62941a7
Sahil-Kumar7253 <darkslayer7253@gmail.com>
2025-02-06 17:10:39 +0530

    UI Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app1/themes/light_mode.dart';
 import 'package:chat_app1/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
